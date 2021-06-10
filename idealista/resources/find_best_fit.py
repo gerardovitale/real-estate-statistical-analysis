e56e1892b85a9ba579bd63e5622c8008e37db6b0
Gerardo Vitale <gerardo.vitale.errico@gmail.com>
2021-06-10 17:35:45 +0200

    last update

--- idealista/resources/find_best_fit.py
+++ idealista/resources/find_best_fit.py
@@ -2,7 +2,6 @@ from typing import List, Tuple
 import warnings
 
 import numpy as np
-import pandas as pd
 from scipy import stats
 from scipy.stats._continuous_distns import _distn_names
 from tqdm.notebook import tqdm
@@ -30,14 +29,13 @@ def fit_distibution_ks(data: np.ndarray, distribution_name: str) \
 
 def find_best_fit_based_ks(data: np.ndarray) -> List[List[str]]:
     distribution_list = get_distribution_name_list()
-    result = []
     result = {'distribution': None, 'd_statistic': 1, 'p_value': 0, 'params': None}
     for distribution_name in tqdm(distribution_list):
         try:
             with warnings.catch_warnings():
                 warnings.filterwarnings('ignore')
                 d_statistic, p_value, params = fit_distibution_ks(data, distribution_name)
-                if result['d_statistic'] > d_statistic and result['p_value'] < p_value:
+                if result['p_value'] < p_value:
                     result['distribution'] = distribution_name
                     result['d_statistic'] = d_statistic
                     result['p_value'] = p_value
@@ -48,46 +46,46 @@ def find_best_fit_based_ks(data: np.ndarray) -> List[List[str]]:
 
 
 @timeout(30)
-def fit_distibution(dist, data, x, y, max_err=False):
+def fit_distibution(dist, data, x, y, err_type='sse'):
     params = dist.fit(data)
     # Separate parts of parameters
     arg, loc, scale = params[:-2], params[-2], params[-1]
     # Calculate fitted PDF and error with fit in dist
     pdf = dist.pdf(x, loc=loc, scale=scale, *arg)
-    sse = np.amax((y - pdf) ** 2) if max_err else np.sum((y - pdf) ** 2)
-    return params, pdf, sse
-# #calculate absolute difference
-# arr_dif_abs = np.abs(cdf - edf)
-# #get max different
-# dn_ks = max(arr_dif_abs)
-# dn_ks
+    if err_type == 'sse':
+        err = np.sum((y - pdf) ** 2) 
+    elif err_type == 'max_sq_err':
+        err = np.amax((y - pdf) ** 2)
+    elif err_type == 'max_err':
+        err = np.amax(np.abs(y - pdf))
+    return pdf, params, err
 
 
-def find_best_distribution(data, ax=None) -> Tuple[str, Tuple[float]]:
-    """Model data by finding best fit distribution to data"""
+def find_best_dist(data, err_type='sse') -> Tuple[str, Tuple[float]]:
+    """
+    Model data by finding best fit distribution to data.
+    Where err_type stands for the method used to calculate the difference 
+    between the distribution studied
+    """
     distribution_list = get_distribution_name_list()
-    # Get histogram of original data
     y, x = np.histogram(data, density=True)
     x = (x + np.roll(x, -1))[:-1] / 2.0
-    best_distribution = stats.norm
-    best_params = (0.0, 1.0)
-    best_sse = np.inf
+    result = {'distribution': None, 'error': np.inf, 'params': None}
     for distribution_name in tqdm(distribution_list):
         try:
-            # Ignore warnings from data that can't be fit
             with warnings.catch_warnings():
                 warnings.filterwarnings('ignore')
                 # print(f"[INFO] evaluating {distribution_name} \
                 #     {distribution_list.index(distribution_name)}/{len(distribution_list)}")
                 dist = getattr(stats, distribution_name)
-                params, pdf, sse = fit_distibution(dist, data, x, y)
-                if best_sse > sse and sse > 0:
-                    best_distribution = distribution_name
-                    best_params = params
-                    best_sse = sse
+                pdf, params, err = fit_distibution(dist, data, x, y, err_type=err_type)
+                if result['error'] > err and err > 0:
+                    result['distribution'] = distribution_name
+                    result['params'] = params
+                    result['error'] = err
         except Exception:
             pass
-    return best_distribution, best_params
+    return result
 
 
 def calculate_pdf(distribution_name: str, params: Tuple[float], size=1000) \
